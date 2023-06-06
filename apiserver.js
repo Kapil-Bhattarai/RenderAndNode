218e989bb3abb9a541b8f95ab2e8dad8e37535ce
Kapil Bhattarai <kapil.edu.bhattarai@gmail.com>
2023-06-06 14:11:07 +1000

    Update apiserver.js

--- apiserver.js
+++ apiserver.js
@@ -14,13 +14,13 @@ const client = new MongoClient(uri, { useUnifiedTopology: true });
 // to allow cross site request
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 app.use(express.json());
 
